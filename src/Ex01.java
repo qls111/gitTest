0c866d7ff076d581af8acc3e779b32a9ea45e594
Choi <choib23@naver.com>
2024-04-17 11:14:32 +0900

    두번 째

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,8 +2,10 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("나의 첫 커밋!");
+		System.out.println("두번째 커밋!");
 	}
 
 }
